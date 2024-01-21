ab80182dead17306cd4d698f198837aaa306766b
Grzegorz Aleksander Klementowski <125459323+grzegorz-aleksander-klementowski@users.noreply.github.com>
2024-01-21 13:21:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 var randomNumber = Math.floor(Math.random()*18)+1;
 
 var password = new Array(20);
-
-password[0] = "Darusia";
-password[1] = "Z³owieszczy PKS";
-password[2] = "Kolorowe jeziorka";
-password[3] = "Gruszczka i Gruszkowe lasy";
-password[4] = "Draru¶ko z Kleciny";
-password[5] = "Jacek Kaczmarski";
-password[6] = "Queen";
-password[7] = "Grze¶ko z Wojcieszowa";
-password[8] = "Darusiowa ¦nie¿ka";
+password[0] = "Zosia";
+password[1] = "PoznaÅ„";
+password[2] = "Gospoda u Grzegorza";
+password[3] = "Pan Tadeusz";
+password[4] = "Anime";
+password[5] = "Wonder Eggs";
+password[6] = "Toh fan";
+password[7] = "Straszne dzieci z podstawÃ³wki";
+password[8] = "Kakao z serduszkiem";
 password[9] = "Droga bez powrotu";
-password[10] = "Surwiwalowe przygody Grzesia i Dareñki";
-password[11] = "Dawano, dawno temu w odleg³ym miejscu";
-password[12] = "Kiedy mogê zabraæ Dareñkê na randkê";
-password[13] = "Surwiwalowe przygody Grzesia i Dareñki";
-password[14] = "Nastêpnym razem postaramy siê przej¶æ mniej ni¿ ostatnio";
-password[15] = "To co powiesz na góry";
-password[16] = "Kiedy mogê zadzwoniæ";
-password[17] = "Jak Ci siê podoba gra";
-password[18] = "Wroc³awska Klecina";
+password[10] = "Cult of the Lamb";
+password[11] = "Dawano, dawno temu w odlegÅ‚ym Poznaniu";
+password[12] = "Kiedy mogÄ™ zabraÄ‡ Zosie na randke";
+password[13] = "Tajemnicze miejsce w Poznaniu";
+password[14] = "NastÃªpnym razem postaramy siÃª przejÂ¶Ã¦ mniej niÂ¿ ostatnio";
+password[15] = "Kiedy idziemy na ÅšnieÅ¼kÄ™";
+password[16] = "Telefon po szkole";
+password[17] = "Szlachcianka";
+password[18] = "Skarbek";
 
 var password1 = password[randomNumber];
 password1 = password1.toUpperCase();
@@ -48,13 +47,13 @@ window.onload = start;
 var letters = new Array(35);
 
 letters[0]="A";
-letters[1]="¡";
+letters[1]="Â¡";
 letters[2]="B";
 letters[3]="C";
-letters[4]="Æ";
+letters[4]="Ã†";
 letters[5]="D";
 letters[6]="E";
-letters[7]="Ê";
+letters[7]="ÃŠ";
 letters[8]="F";
 letters[9]="G";
 letters[10]="H";
@@ -62,17 +61,17 @@ letters[11]="I";
 letters[12]="J";
 letters[13]="K";
 letters[14]="L";
-letters[15]="£";
+letters[15]="Â£";
 letters[16]="M";
 letters[17]="N";
-letters[18]="Ñ";
+letters[18]="Ã‘";
 letters[19]="O";
-letters[20]="Ó";
+letters[20]="Ã“";
 letters[21]="P";
 letters[22]="Q";
 letters[23]="R";
 letters[24]="S";
-letters[25]="¦";
+letters[25]="Â¦";
 letters[26]="T";
 letters[27]="U";
 letters[28]="V";
@@ -80,8 +79,8 @@ letters[29]="W";
 letters[30]="X";
 letters[31]="Y";
 letters[32]="Z";
-letters[33]="¯";
-letters[34]="¬";
+letters[33]="Â¯";
+letters[34]="Â¬";
 
 function start()
 {
@@ -146,13 +145,13 @@ function check(no)
         //Win
         if(password1==password1v2)
         {
-            document.getElementById("alphabet").innerHTML = 'Tak jest! To jest poprawne has³o: '+password1+' </br></br><span class="reset" onclick="location.reload()"> JESZCZE RAZ?</span>';
+            document.getElementById("alphabet").innerHTML = 'Tak jest! To jest poprawne hasÂ³o: '+password1+' </br></br><span class="reset" onclick="location.reload()"> JESZCZE RAZ?</span>';
         }
     }
         //Lose
         if(ile_skuch>9)
         {
-            document.getElementById("alphabet").innerHTML = 'Przegra³e¶! To jest poprawne has³o: '+password1+' </br></br><span class="reset" onclick="location.reload()"> JESZCZE RAZ?</span>';
+            document.getElementById("alphabet").innerHTML = 'PrzegraÂ³eÂ¶! To jest poprawne hasÂ³o: '+password1+' </br></br><span class="reset" onclick="location.reload()"> JESZCZE RAZ?</span>';
         }
     write_down_password();
 }
